5926af6483d796345f11dae35f0679a0bbfeb436
ali ghazi <ali.msgh@gmail.com>
2011-06-06 17:58:27 +0430

    -migrating to sdl

--- src/src/m.cpp
+++ src/src/m.cpp
@@ -3,6 +3,7 @@
 #include <gl/freeglut.h>
 #include "resource.h"
 #include "MaarGame.h"
+#include "SDL.h"
 
 using namespace MAAR;
 
@@ -60,40 +61,11 @@ static void SpecialKey(int key, int x, int y)
 	MaarGame::GetInstance()->SetLastPressedKey(key);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int nShowCmd)
+int WINAPI	WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
 {
-	int argc;
-	LPWSTR *argv= CommandLineToArgvW(GetCommandLine(),&argc);
-
-	glutInit(&argc,(char **)(argv));
-
-	glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE);
-	glutInitWindowSize( MaarGame::windowDimensions.x, MaarGame::windowDimensions.y );
-	glutCreateWindow( "" );
-
-	// Dither is on by default but not needed, disable it.
-	glDisable( GL_DITHER );
-
-	assert( glGetError() == GL_NO_ERROR );
-
-	// Register our display and resize callbacks with GLUT.
-	glutDisplayFunc( display );
-	glutReshapeFunc( reshape );
-
-	// Create a right-mouse menu to allow users to exit.
-	int mainMenu = glutCreateMenu( mainMenuCB );
-	glutAddMenuEntry( "Quit", QUIT_VALUE );
-	glutAttachMenu( GLUT_RIGHT_BUTTON );
-
-	int updateInterval=MaarGame::GetInstance()->updateInterval;
-
-	glutTimerFunc(updateInterval,timer_exp, updateInterval);
-	glutSpecialFunc(SpecialKey);
-
-	char title[64];
-	sprintf_s(title,64,"OpenGL Version %s",glGetString( GL_VERSION ));
-	glutSetWindowTitle(title);
-    // Loop for events.
-    glutMainLoop();
-    return 0;
+	if ( SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0 ) {
+		fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+	atexit(SDL_Quit);
 }
